56a2c276b9fc44d2ee6398b99ac5760ec5ddf7a4
dongdongjie <384168902@qq.com>
2019-11-27 10:58:19 +0800

    使用Ant-Design-Pro初始化项目

---
+++ src/models/setting.js
@@ -0,0 +1,41 @@
+import defaultSettings from '../../config/defaultSettings';
+
+const updateColorWeak = colorWeak => {
+  const root = document.getElementById('root');
+
+  if (root) {
+    root.className = colorWeak ? 'colorWeak' : '';
+  }
+};
+
+const SettingModel = {
+  namespace: 'settings',
+  state: defaultSettings,
+  reducers: {
+    getSetting(state = defaultSettings) {
+      const setting = {};
+      const urlParams = new URL(window.location.href);
+      Object.keys(state).forEach(key => {
+        if (urlParams.searchParams.has(key)) {
+          const value = urlParams.searchParams.get(key);
+          setting[key] = value;
+        }
+      });
+      const { colorWeak } = setting;
+      updateColorWeak(!!colorWeak);
+      return { ...state, ...setting };
+    },
+
+    changeSetting(state = defaultSettings, { payload }) {
+      const { colorWeak, contentWidth } = payload;
+
+      if (state.contentWidth !== contentWidth && window.dispatchEvent) {
+        window.dispatchEvent(new Event('resize'));
+      }
+
+      updateColorWeak(!!colorWeak);
+      return { ...state, ...payload };
+    },
+  },
+};
+export default SettingModel;
